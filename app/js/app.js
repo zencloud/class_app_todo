6e28051fc890cc4671ab69adcb4881fafd2b00d4
zencloud <mryanmill@gmail.com>
2019-06-15 16:38:37 -0600

    Initial push

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+
+
+// App Data Object
+const appData = {
+
+    // Local Storage Key
+    storageKey: "itemLibrary",
+
+    // Main Todo List Library
+    itemLibrary: []
+}
+
+// First App Load
+app_initialize();
+app_render_list();
---
+++ app/js/app_functions.js
@@ -0,0 +1,85 @@
+// Main Functions
+
+
+// --- App Initialize
+function app_initialize() {
+
+    // Check if we item library exists: Create it or copy it
+    if (localStorage.getItem('itemLibrary') === null) {
+        localStorage.setItem('itemLibrary', JSON.stringify(appData.itemLibrary));
+    } 
+    else {
+        appData.itemLibrary = JSON.parse(localStorage.getItem('itemLibrary'));
+    }
+
+    // Initialize Button Listeners
+    $('#btn-item-add').on('click', function() {
+        
+        // Get Input
+        let inputItem = $('#input-item').val();
+
+        // Input validation: Is the input field empty, if so early exit with null;
+        if (inputItem === '') { return null; }
+
+        appData.itemLibrary.push(inputItem);
+        localStorage.setItem(appData.storageKey, JSON.stringify(appData.itemLibrary));
+
+        // Clear Input Field
+        $('#input-item').val('');
+
+        // Render List
+        app_render_list();
+    });
+
+}
+
+// Remove item from last list
+function app_item_remove(index) {
+
+    appData.itemLibrary.splice(index, 1);
+    localStorage.setItem(appData.storageKey, JSON.stringify(appData.itemLibrary));
+
+    app_render_list();
+
+}
+
+// Render Item List
+function app_render_list() {
+
+    // Clear Initial Div because we're putting stuff into it regardless
+    $('#item-container').empty();
+    
+    // Check if library has items in it.
+    // Loop through and render each item on the dom.
+    if (appData.itemLibrary.length > 0) {
+        appData.itemLibrary.forEach(function(itemValue, index){
+            
+            // Draw up template
+            let myHTML = `
+            <div class="item-cell">
+                <div class="item-done" onclick="app_item_remove(${index})" id="btn-item-remove">
+                    <i class="fas fa-check-circle"></i>
+                </div>
+                <p class="item-details">
+                    ${itemValue}
+                </p>
+            </div>`;
+            
+            // Append Item
+            
+            $('#item-container').append(myHTML);
+        });
+    }
+    else {
+
+
+        // Draw up template
+        let myHTML = `
+                <p class="item-empty">
+                    List is empty!
+                </p>`;
+            
+        // Append Item
+        $('#item-container').append(myHTML);
+    } 
+}
